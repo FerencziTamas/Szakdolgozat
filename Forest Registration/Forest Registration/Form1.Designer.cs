9fe6756bf5135cd441ed3c60be4ae879d78cb7e9
Tamás Ferenczi <darius.517.ft@gmail.com>
2020-01-28 19:48:56 +0100

    Vevők kezelése felvíve formba

---
+++ Forest Registration/Forest Registration/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Forest_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.erdőkKezeléseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.értékesítésreVáróFákToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vevőkKezeléseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.erdőkKezeléseToolStripMenuItem,
+            this.értékesítésreVáróFákToolStripMenuItem,
+            this.vevőkKezeléseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(20, 60);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1106, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // erdőkKezeléseToolStripMenuItem
+            // 
+            this.erdőkKezeléseToolStripMenuItem.Name = "erdőkKezeléseToolStripMenuItem";
+            this.erdőkKezeléseToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.erdőkKezeléseToolStripMenuItem.Text = "Erdők kezelése";
+            // 
+            // értékesítésreVáróFákToolStripMenuItem
+            // 
+            this.értékesítésreVáróFákToolStripMenuItem.Name = "értékesítésreVáróFákToolStripMenuItem";
+            this.értékesítésreVáróFákToolStripMenuItem.Size = new System.Drawing.Size(129, 20);
+            this.értékesítésreVáróFákToolStripMenuItem.Text = "Értékesítésre váró fák";
+            // 
+            // vevőkKezeléseToolStripMenuItem
+            // 
+            this.vevőkKezeléseToolStripMenuItem.Name = "vevőkKezeléseToolStripMenuItem";
+            this.vevőkKezeléseToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.vevőkKezeléseToolStripMenuItem.Text = "Vevők kezelése";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 600);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem erdőkKezeléseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem értékesítésreVáróFákToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vevőkKezeléseToolStripMenuItem;
+    }
+}
+
