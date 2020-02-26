c7afd4b1b3769037128618bac034b3a363586ff7
Tamás Ferenczi <darius.517.ft@gmail.com>
2020-02-26 19:59:05 +0100

    Szamla.cs be kell fejezni

---
+++ Forest Registration/Forest Registration/modell/Szamlak.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forest_Registration.modell
+{
+    class Szamlak
+    {
+        private string szamlaszam;
+        private string vevonev;
+        private int nettoar;
+        private int bruttoar;
+        
+    }
+}
