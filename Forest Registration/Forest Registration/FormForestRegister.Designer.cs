55ce86c3cbf8e5ae7f78956aa197cf6c8a5cb441
Tamás Ferenczi <darius.517.ft@gmail.com>
2020-02-27 13:48:51 +0100

    Új form hozzáadva metro frameworkkel

--- Forest Registration/Forest Registration/FormForestRegister.Designer.cs
+++ Forest Registration/Forest Registration/FormForestRegister.Designer.cs
@@ -29,26 +29,186 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.erdőkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.erdőkKiírtatásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.erdőToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fafajokKimutatásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.számlákToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.számlákKiíratásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.újSzámlaFelvételeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vevőkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vevőkKiíratásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aProgramInformációiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.tabPageErdok = new System.Windows.Forms.TabPage();
+            this.tabPageErdogazdalkodok = new System.Windows.Forms.TabPage();
+            this.tabPageFafajok = new System.Windows.Forms.TabPage();
+            this.tabPageVevok = new System.Windows.Forms.TabPage();
+            this.tabPageSzamlak = new System.Windows.Forms.TabPage();
+            this.tabPageUjSzamla = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            this.metroTabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.Color.LightGray;
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.erdőkToolStripMenuItem,
+            this.számlákToolStripMenuItem,
+            this.vevőkToolStripMenuItem,
+            this.aProgramInformációiToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(20, 60);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1177, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1491, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // erdőkToolStripMenuItem
+            // 
+            this.erdőkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.erdőkKiírtatásaToolStripMenuItem,
+            this.erdőToolStripMenuItem,
+            this.fafajokKimutatásaToolStripMenuItem});
+            this.erdőkToolStripMenuItem.Name = "erdőkToolStripMenuItem";
+            this.erdőkToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.erdőkToolStripMenuItem.Text = "Erdők";
+            // 
+            // erdőkKiírtatásaToolStripMenuItem
+            // 
+            this.erdőkKiírtatásaToolStripMenuItem.Name = "erdőkKiírtatásaToolStripMenuItem";
+            this.erdőkKiírtatásaToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.erdőkKiírtatásaToolStripMenuItem.Text = "Erdők kiíratása";
+            // 
+            // erdőToolStripMenuItem
+            // 
+            this.erdőToolStripMenuItem.Name = "erdőToolStripMenuItem";
+            this.erdőToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.erdőToolStripMenuItem.Text = "Erdőgazdálkodók kiíratása";
+            // 
+            // fafajokKimutatásaToolStripMenuItem
+            // 
+            this.fafajokKimutatásaToolStripMenuItem.Name = "fafajokKimutatásaToolStripMenuItem";
+            this.fafajokKimutatásaToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.fafajokKimutatásaToolStripMenuItem.Text = "Fafajok kimutatása";
+            // 
+            // számlákToolStripMenuItem
+            // 
+            this.számlákToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.számlákKiíratásaToolStripMenuItem,
+            this.újSzámlaFelvételeToolStripMenuItem});
+            this.számlákToolStripMenuItem.Name = "számlákToolStripMenuItem";
+            this.számlákToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.számlákToolStripMenuItem.Text = "Számlák";
+            // 
+            // számlákKiíratásaToolStripMenuItem
+            // 
+            this.számlákKiíratásaToolStripMenuItem.Name = "számlákKiíratásaToolStripMenuItem";
+            this.számlákKiíratásaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.számlákKiíratásaToolStripMenuItem.Text = "Számlák kiíratása";
+            // 
+            // újSzámlaFelvételeToolStripMenuItem
+            // 
+            this.újSzámlaFelvételeToolStripMenuItem.Name = "újSzámlaFelvételeToolStripMenuItem";
+            this.újSzámlaFelvételeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.újSzámlaFelvételeToolStripMenuItem.Text = "Új számla felvétele";
+            // 
+            // vevőkToolStripMenuItem
+            // 
+            this.vevőkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vevőkKiíratásaToolStripMenuItem});
+            this.vevőkToolStripMenuItem.Name = "vevőkToolStripMenuItem";
+            this.vevőkToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.vevőkToolStripMenuItem.Text = "Vevők";
+            // 
+            // vevőkKiíratásaToolStripMenuItem
+            // 
+            this.vevőkKiíratásaToolStripMenuItem.Name = "vevőkKiíratásaToolStripMenuItem";
+            this.vevőkKiíratásaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vevőkKiíratásaToolStripMenuItem.Text = "Vevők kiíratása";
+            // 
+            // aProgramInformációiToolStripMenuItem
+            // 
+            this.aProgramInformációiToolStripMenuItem.Name = "aProgramInformációiToolStripMenuItem";
+            this.aProgramInformációiToolStripMenuItem.Size = new System.Drawing.Size(140, 20);
+            this.aProgramInformációiToolStripMenuItem.Text = "A program információi";
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.tabPageErdok);
+            this.metroTabControl1.Controls.Add(this.tabPageErdogazdalkodok);
+            this.metroTabControl1.Controls.Add(this.tabPageFafajok);
+            this.metroTabControl1.Controls.Add(this.tabPageSzamlak);
+            this.metroTabControl1.Controls.Add(this.tabPageUjSzamla);
+            this.metroTabControl1.Controls.Add(this.tabPageVevok);
+            this.metroTabControl1.Location = new System.Drawing.Point(20, 87);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(1491, 649);
+            this.metroTabControl1.TabIndex = 1;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // tabPageErdok
+            // 
+            this.tabPageErdok.Location = new System.Drawing.Point(4, 38);
+            this.tabPageErdok.Name = "tabPageErdok";
+            this.tabPageErdok.Size = new System.Drawing.Size(1483, 607);
+            this.tabPageErdok.TabIndex = 0;
+            this.tabPageErdok.Text = "Erdők kiíratása";
+            // 
+            // tabPageErdogazdalkodok
+            // 
+            this.tabPageErdogazdalkodok.Location = new System.Drawing.Point(4, 38);
+            this.tabPageErdogazdalkodok.Name = "tabPageErdogazdalkodok";
+            this.tabPageErdogazdalkodok.Size = new System.Drawing.Size(1483, 607);
+            this.tabPageErdogazdalkodok.TabIndex = 1;
+            this.tabPageErdogazdalkodok.Text = "Erdőgazdálkodók kiíratása";
+            // 
+            // tabPageFafajok
+            // 
+            this.tabPageFafajok.Location = new System.Drawing.Point(4, 38);
+            this.tabPageFafajok.Name = "tabPageFafajok";
+            this.tabPageFafajok.Size = new System.Drawing.Size(1483, 607);
+            this.tabPageFafajok.TabIndex = 2;
+            this.tabPageFafajok.Text = "Fafajok kimutatása";
+            // 
+            // tabPageVevok
+            // 
+            this.tabPageVevok.Location = new System.Drawing.Point(4, 38);
+            this.tabPageVevok.Name = "tabPageVevok";
+            this.tabPageVevok.Size = new System.Drawing.Size(1483, 607);
+            this.tabPageVevok.TabIndex = 3;
+            this.tabPageVevok.Text = "Vevők kiíratása";
+            // 
+            // tabPageSzamlak
+            // 
+            this.tabPageSzamlak.Location = new System.Drawing.Point(4, 38);
+            this.tabPageSzamlak.Name = "tabPageSzamlak";
+            this.tabPageSzamlak.Size = new System.Drawing.Size(1483, 607);
+            this.tabPageSzamlak.TabIndex = 4;
+            this.tabPageSzamlak.Text = "Számlák kiíratása";
+            // 
+            // tabPageUjSzamla
+            // 
+            this.tabPageUjSzamla.Location = new System.Drawing.Point(4, 38);
+            this.tabPageUjSzamla.Name = "tabPageUjSzamla";
+            this.tabPageUjSzamla.Size = new System.Drawing.Size(1483, 607);
+            this.tabPageUjSzamla.TabIndex = 5;
+            this.tabPageUjSzamla.Text = "Új számla felvétele";
             // 
             // FormForestRegister
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1217, 530);
+            this.ClientSize = new System.Drawing.Size(1531, 740);
+            this.Controls.Add(this.metroTabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormForestRegister";
-            this.Text = "FormForestRegister";
+            this.Text = "Forest Register";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.metroTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +217,22 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem erdőkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem erdőkKiírtatásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem erdőToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fafajokKimutatásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem számlákToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem számlákKiíratásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem újSzámlaFelvételeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vevőkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vevőkKiíratásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aProgramInformációiToolStripMenuItem;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.TabPage tabPageErdok;
+        private System.Windows.Forms.TabPage tabPageErdogazdalkodok;
+        private System.Windows.Forms.TabPage tabPageFafajok;
+        private System.Windows.Forms.TabPage tabPageSzamlak;
+        private System.Windows.Forms.TabPage tabPageUjSzamla;
+        private System.Windows.Forms.TabPage tabPageVevok;
     }
 }
